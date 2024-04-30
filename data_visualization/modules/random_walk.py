1329cdf1ebafe4a54b43554c20af9b83b0200789
almanzanas <alvaro.manzanas@outlook.com>
2024-04-30 16:02:57 +0200

    Updating exercises

--- data_visualization/modules/random_walk.py
+++ data_visualization/modules/random_walk.py
@@ -18,27 +18,35 @@ class RandomWalk:
     ### Choosing Directions
     def fill_walk(self):
         """Calculate all the points in the walk"""
+        x_step = self.get_step()
+        y_step = self.get_step()
 
-        # Keep taking steps until the walk reaches 'num_points'
-        while len(self.x_val) < self.num_points:
-
-            # Decide which direction to go, and how far to go (right or left, up or down, and how far)
-            x_direction = choice([1, -1]) # 1 = right; -1 = left
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance # positive move up, negative move down
-
-            # Reject moves that go nowhere
-            if x_step == 0 and y_step == 0:
-                continue
-
+        if x_step is not None and y_step is not None:
             # Calculate the new position
             y = self.y_val[-1] + y_step
             x = self.x_val[-1] + x_step
 
             self.x_val.append(x)
             self.y_val.append(y)
+        else:
+            self.get_step()
             
+    def get_step(self):
+        """"Direction and distance of each step"""
+
+        # Keep taking steps until the walk reaches 'num_points'
+        while len(self.x_val) < self.num_points:
+
+            # Decide which direction to go, and how far to go (right or left, up or down, and how far)
+            x_direction = choice([-1]) # 1 = right; -1 = left
+            x_distance = choice([0, 1, 2, 3, 4, 5, 7])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([1])
+            y_distance = choice([0, 1, 2, 3, 4, 5, 7])
+            y_step = y_direction * y_distance # positive move up, negative move down
+
+            # Reject moves that go nowhere
+            if x_step == 0 and y_step == 0:
+                continue
+        
